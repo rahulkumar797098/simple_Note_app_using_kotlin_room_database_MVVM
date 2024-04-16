2dfde215c1c6d3611d8b1fc4079afa96b209d5a8
Rahul Kumar Sahu <rkrahulroy151617@gmail.com>
2024-04-16 21:55:12 +0530

    commit

---
+++ app/src/main/java/com/example/notesapp/Database/NotesRepository.kt
@@ -0,0 +1,23 @@
+package com.example.notesapp.Database
+
+
+import androidx.lifecycle.LiveData
+import com.example.notesapp.Models.Note
+
+class NotesRepository(private val noteDao: NoteDao) {
+
+    val allNotes : LiveData<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insertNote(note : Note) {
+        noteDao.insertNote(note)
+    }
+    suspend fun deleteNote(note: Note){
+        noteDao.deleteNote(note)
+
+
+    }
+
+    suspend fun updateNote(note: Note){
+        noteDao.updateNote(note.id , note.title , note.note)
+    }
+}
